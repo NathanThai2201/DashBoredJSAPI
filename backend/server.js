dff96629ffe3e9e1abb8d6cdc4725dc0740c4de2
NathanThai2201 <thainhatanh2201@gmail.com>
2025-02-03 16:02:35 -0700

    cors update

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express, { application } from 'express'; 
+import cors from 'cors';
 import { connectDB } from "./config/db.js";
 import dotenv from 'dotenv';
 import postRoutes from "./routes/post.route.js";
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 5000
 
 const __dirname = path.resolve();
 app.use(express.json()); // allow JSON data in req.body
+
+app.use(cors({ origin: 'https://nathanthai2201.github.io/portfolio/' }));
 
 app.use("/api/posts",postRoutes);
 
