45769b70794da8deb91c640d72aceec5ded4e59c
NathanThai2201 <thainhatanh2201@gmail.com>
2025-02-06 14:23:29 -0700

    up

--- backend/routes/cardarray.route.js
+++ backend/routes/cardarray.route.js
@@ -6,8 +6,8 @@ const router = express.Router();
 //
 //  ENDPOINTS
 //
-// router.get("/", getCardArrays);
-router.get("/:id", getCardArray);
+router.get("/", getCardArrays);
+// router.get("/:id", getCardArray);
 router.post("/", createCardArray);
 router.put("/:id", updateCardArray);
 router.delete("/:id", deleteCardArray);
